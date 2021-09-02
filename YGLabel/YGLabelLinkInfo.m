7d6a0a3899b0171f8d97407654925c560334f87d
leo <199320701@qq.com>
2021-09-02 15:59:22 +0800

    Add 封装link信息

--- YGLabel/YGLabelLinkInfo.m
+++ YGLabel/YGLabelLinkInfo.m
@@ -6,4 +6,23 @@
 
 @implementation YGLabelLinkInfo
 
++ (YGLabelLinkInfo *)linkInfo:(id)content range:(NSRange)range {
+    YGLabelLinkInfo *info = [[YGLabelLinkInfo alloc] init];
+    info.content = content;
+    info.range = range;
+    return info;
+}
+
+- (BOOL)isEqual:(id)other
+{
+    if (other == self) {
+        return YES;
+    } else if ([other isKindOfClass:[self class]]) {
+        YGLabelLinkInfo *_other = (YGLabelLinkInfo *)other;
+        return [_other.content isEqual:self.content] && NSEqualRanges(_other.range, self.range);
+    } else {
+        return NO;
+    }
+}
+
 @end
