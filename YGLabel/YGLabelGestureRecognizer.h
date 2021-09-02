752c4b479e2450df24b01701f580f95640a15347
leo <199320701@qq.com>
2021-09-02 17:41:11 +0800

    Add: 增加Label的点击手势

---
+++ YGLabel/YGLabelGestureRecognizer.h
@@ -0,0 +1,16 @@
+//
+//  YGLabelGestureRecognizer.h
+//  YGLabel
+//
+//  Created by leo on 2021/9/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YGLabelGestureRecognizer : UIGestureRecognizer
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YGLabel/YGLabelGestureRecognizer.m
@@ -0,0 +1,12 @@
+//
+//  YGLabelGestureRecognizer.m
+//  YGLabel
+//
+//  Created by leo on 2021/9/2.
+//
+
+#import "YGLabelGestureRecognizer.h"
+
+@implementation YGLabelGestureRecognizer
+
+@end
