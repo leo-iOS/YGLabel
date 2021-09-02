5a9078768dd91a5dade831700097fe5584a41651
leo <199320701@qq.com>
2021-09-02 11:50:45 +0800

    Add 增加Defines里面一些配置

--- YGLabel/VGLabelDefines.h
+++ YGLabel/VGLabelDefines.h
@@ -1,12 +1,33 @@
 //
 //  VGLabelDefines.h
 //  Pods
-//
-//  Created by leo on 2021/9/2.
-//
 
 #ifndef VGLabelDefines_h
 #define VGLabelDefines_h
+#import <pthread.h>
 
+#define YGLabelDefaultFont [UIFont systemFontOfSize:14]
+
+#define YGLabelDefaultTextColor [UIColor blackColor]
+
+#define YGLabelDefaultLinkTextColor [UIColor blueColor]
+
+static inline BOOL VPFloatIsEqual(CGFloat f1, CGFloat f2) {
+    return ABS(f1 - f2) < DBL_EPSILON;
+}
+
+static inline void dispatch_async_on_main_queue(void (^block)(void)) {
+    if (pthread_main_np()) {
+        block();
+    } else {
+        dispatch_async(dispatch_get_main_queue(), block);
+    }
+}
+
+typedef NS_ENUM(NSUInteger, YGTextAttachmentAlignment) {
+    YGTextAttachmentAlignmentTop,
+    YGTextAttachmentAlignmentCenter,
+    YGTextAttachmentAlignmentBottom,
+};
 
 #endif /* VGLabelDefines_h */
