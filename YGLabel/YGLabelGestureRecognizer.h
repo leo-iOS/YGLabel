e62b1e978004ad099b9b7befaa20b36e9b6d7175
leo <199320701@qq.com>
2021-09-02 18:03:56 +0800

    Add: 增加手势代理方法

--- YGLabel/YGLabelGestureRecognizer.h
+++ YGLabel/YGLabelGestureRecognizer.h
@@ -1,15 +1,25 @@
 //
 //  YGLabelGestureRecognizer.h
 //  YGLabel
-//
-//  Created by leo on 2021/9/2.
-//
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@protocol YGLabelGestureRecognizerDelegate <NSObject>
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches gesture:(UIGestureRecognizer *)gesture;
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches gesture:(UIGestureRecognizer *)gesture;
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches gesture:(UIGestureRecognizer *)gesture;
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches gesture:(UIGestureRecognizer *)gesture;
+
+@end
 
 @interface YGLabelGestureRecognizer : UIGestureRecognizer
+
+@property (nonatomic, weak) id<YGLabelGestureRecognizerDelegate> yg_delegate;
 
 @end
 
--- YGLabel/YGLabelGestureRecognizer.m
+++ YGLabel/YGLabelGestureRecognizer.m
@@ -1,12 +1,33 @@
 //
 //  YGLabelGestureRecognizer.m
 //  YGLabel
-//
-//  Created by leo on 2021/9/2.
-//
 
 #import "YGLabelGestureRecognizer.h"
 
 @implementation YGLabelGestureRecognizer
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.yg_delegate && [self.yg_delegate respondsToSelector:@selector(touchesBegan:gesture:)]) {
+        [self.yg_delegate touchesBegan:touches gesture:self];
+    }
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.yg_delegate && [self.yg_delegate respondsToSelector:@selector(touchesMoved:gesture:)]) {
+        [self.yg_delegate touchesMoved:touches gesture:self];
+    }
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.yg_delegate && [self.yg_delegate respondsToSelector:@selector(touchesEnded:gesture:)]) {
+        [self.yg_delegate touchesEnded:touches gesture:self];
+    }
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.yg_delegate && [self.yg_delegate respondsToSelector:@selector(touchesCancelled:gesture:)]) {
+        [self.yg_delegate touchesCancelled:touches gesture:self];
+    }
+}
 
 @end
