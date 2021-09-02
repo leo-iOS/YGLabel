9155992e7c455af2d622818993bc50d65f3266aa
leo <199320701@qq.com>
2021-09-02 11:44:40 +0800

    Add 增加NSMutableString的扩展方法

--- YGLabel/NSMutableAttributedString+YG.h
+++ YGLabel/NSMutableAttributedString+YG.h
@@ -2,10 +2,28 @@
 //  NSMutableAttributedString+YG.h
 
 #import <Foundation/Foundation.h>
+#import <CoreText/CoreText.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSMutableAttributedString (YG)
+
+- (void)yg_setTextColor:(UIColor *)color;
+
+- (void)yg_setTextColor:(UIColor *)color range:(NSRange)range;
+
+- (void)yg_setFont:(UIFont *)font;
+
+- (void)yg_setFont:(UIFont *)font range:(NSRange)range;
+
+- (void)yg_setUnderlineStyle:(CTUnderlineStyle)style;
+
+
+- (void)yg_setUnderlineStyle:(CTUnderlineStyle)style
+                 modifier:(CTUnderlineStyleModifiers)modifier
+                       range:(NSRange)range;
+
+- (void)yg_setEndIndentOffset:(CGFloat)endIndentOffset;
 
 @end
 
--- YGLabel/NSMutableAttributedString+YG.m
+++ YGLabel/NSMutableAttributedString+YG.m
@@ -6,4 +6,56 @@
 
 @implementation NSMutableAttributedString (YG)
 
+- (void)yg_setTextColor:(UIColor *)color {
+    [self yg_setTextColor:color range:NSMakeRange(0, self.length)];
+}
+
+- (void)yg_setTextColor:(UIColor *)color range:(NSRange)range {
+    [self removeAttribute:NSForegroundColorAttributeName range:range];
+    if (color)
+    {
+        [self addAttribute:NSForegroundColorAttributeName
+                     value:color
+                     range:range];
+    }
+}
+
+- (void)yg_setFont:(UIFont *)font {
+    [self yg_setFont:font range:NSMakeRange(0, self.length)];
+}
+
+- (void)yg_setFont:(UIFont *)font range:(NSRange)range {
+    if (font)
+    {
+        [self removeAttribute:NSFontAttributeName range:range];
+        if (font)
+        {
+            [self addAttribute:NSFontAttributeName value:font range:range];
+        }
+    }
+}
+
+
+- (void)yg_setUnderlineStyle:(CTUnderlineStyle)style
+{
+    [self yg_setUnderlineStyle:style
+                   modifier:kCTUnderlinePatternSolid
+                      range:NSMakeRange(0, self.length)];
+}
+
+- (void)yg_setUnderlineStyle:(CTUnderlineStyle)style
+                 modifier:(CTUnderlineStyleModifiers)modifier
+                    range:(NSRange)range
+{
+    [self removeAttribute:(NSString *)kCTUnderlineColorAttributeName range:range];
+    [self addAttribute:(NSString *)kCTUnderlineStyleAttributeName
+                 value:[NSNumber numberWithInt:(style|modifier)]
+                 range:range];
+    
+}
+
+#pragma mark - to do
+- (void)yg_setEndIndentOffset:(CGFloat)endIndentOffset {
+    
+}
 @end
